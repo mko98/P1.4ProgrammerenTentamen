2e3c3edbb39caab22eccf573d29e662f512633b4
Max Koning <maxkoning@casema.nl>
2017-06-20 12:11:55 +0200

    Werkende android

--- programmerenTentamen/app/src/main/java/com/example/max/programmerententamen/service/Constants.java
+++ programmerenTentamen/app/src/main/java/com/example/max/programmerententamen/service/Constants.java
@@ -1,7 +1,7 @@
 package com.example.max.programmerententamen.service;
 
 public class Constants {
-    public static final String URL_LOGIN = "https://tentamenserver.herokuapp.com/api/v1/login";
-    public static final String URL_FILMS = "https://tentamenserver.herokuapp.com/api/v1/films/20&20";
-    public static final String URL_REGISTER = "https://tentamenserver.herokuapp.com/api/v1/register";
+    public static final String URL_LOGIN = "https://enigmatic-beyond-43966.herokuapp.com/api/v1/login";
+    public static final String URL_FILMS = "https://enigmatic-beyond-43966.herokuapp.com/api/v1/films/20&20";
+    public static final String URL_REGISTER = "https://enigmatic-beyond-43966.herokuapp.com/api/v1/register";
 }
